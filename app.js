75343b2041ec2b00fd0451d920c32a7aa8999175
陆玄青 <lu.xuanqing@outlook.com>
2017-01-17 14:52:55 +0800

    admin page

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// 设置模板引擎
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+// 静态资源
+app.use(express.static('public'))
+
+// 路由设置
 app.get('/', (req, res) => {
-  res.send('<h1 style="text-align:center">Hello World</h1>')
+  console.log(req.method + ': ' + req.url)
+  res.send('<h1>Hello World</h1>')
+})
+app.get('/admin', (req, res) => {
+  res.render('admin.pug', {
+    title: '食谱管理'
+  })
 })
 
+// 监听端口
 var server = app.listen(port, () => {
-  var host = server.address().address
   var port = server.address().port
-  console.log(`App listening at http://${host}:${port}`)
+  console.log(`App server is listening at http://localhost:${port}`)
 })
