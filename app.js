3efec87076e8e7f593de3136b60e447ca4bf2bc5
陆玄青 <lu.xuanqing@outlook.com>
2017-01-17 18:08:35 +0800

    recipe editor模板

--- app.js
+++ app.js
@@ -15,9 +15,52 @@ app.get('/', (req, res) => {
   console.log(req.method + ': ' + req.url)
   res.send('<h1>Hello World</h1>')
 })
-app.get('/admin', (req, res) => {
-  res.render('admin.pug', {
-    title: '食谱管理'
+// 食谱管理主页
+app.get('/admin/recipes', (req, res) => {
+  res.render('recipes.pug', {
+    title: '食谱管理',
+    recipes:[{
+      _id: '90IJ2LASD0F2M',
+      name: '金汤鱼圆&藕夹',
+      content: '一碗清汤鱼圆配着雪菜春笋，三弦两拨清净，烟花三月旖旎，倒也是说的差不多了。',
+      meta: {
+        createdAt: 1484639665291,
+        updateAt: 1484639715723,
+      }
+    },{
+      _id: 'ASDFLASD03SF',
+      name: '不刻意的大盘鸡',
+      content: '你没的狡辩的，下车之后，你望向我靠坐的那格车窗，也没急着走。',
+      meta: {
+        createdAt: 1484639665291,
+        updateAt: 1484639715723,
+      }
+    }]
+  })
+})
+// 创建新食谱
+app.get('/admin/recipes/create', (req, res) => {
+  res.render('recipe-editor', {
+    title: '食谱编辑',
+    recipe: {
+      name: '',
+      content: '',
+    }
+  })
+})
+// 食谱详情编辑
+app.get('/admin/recipes/:id', (req, res) => {
+  res.render('recipe-editor', {
+    title: '食谱编辑',
+    recipe: {
+      _id: 'ASDFLASD03SF',
+      name: '不刻意的大盘鸡',
+      content: '你没的狡辩的，下车之后，你望向我靠坐的那格车窗，也没急着走。',
+      meta: {
+        createdAt: 1484639665291,
+        updateAt: 1484639715723,
+      }
+    }
   })
 })
 
