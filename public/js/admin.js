de3a1a13f3712fbe7a1f32f7cad202e247fa0f32
陆玄青 <lu.xuanqing@outlook.com>
2017-01-18 14:45:08 +0800

    修复兼容性问题

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,15 +1,15 @@
-$(() => {
-  $('.del').on('click', (event) => {
-    const target = $(event.target)
-    const id = target.data('id')
-    console.log(id)
+console.log('admin.js loaded')
+$(document).ready(function() {
+  console.log('jq loaded')
+  $('.del').on('click', function(event) {
+    var target = $(event.target)
+    var id = target.data('id')
     var tr = $('.recipe-id-' + id)
-    console.log(tr)
     $.ajax({
       type: 'DELETE',
       url: '/admin/recipes?id=' + id
     })
-    .done((results) => {
+    .done(function(results) {
       if (results.success === 1 && tr.length > 0) {
         tr.remove()
       }
